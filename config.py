c895e09aa4ddc94c8df52f96336549dd47d65fab
Isaac Hodes <isaachodes@gmail.com>
2014-06-04 18:49:56 -0400

    ready to deploy

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 
 
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:////tmp/test.db') # TODO(ihodes): remove this.
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '')
 PORT = os.environ.get('PORT', 5000)
 DEBUG = os.environ.get('DEBUG', True) # TODO(ihodes): unsafe, but this doesn't
                                       # matter here.
--- run.py
+++ run.py
@@ -2,4 +2,6 @@ from cycledash import app
 
 
 if __name__ == '__main__':
-    app.run(debug=app.config['DEBUG'], port=app.config['PORT'])
+    app.run(debug=app.config['DEBUG'],
+            port=app.config['PORT'],
+            host='0.0.0.0')
