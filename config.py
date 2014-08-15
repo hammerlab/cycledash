38c2e8877936f5d5393e53a176a180f3cef9552c
Isaac Hodes <isaachodes@gmail.com>
2014-08-15 11:12:02 -0400

    Convert PORT to int on config

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '')
-PORT = os.environ.get('PORT', 5000)
+PORT = int(os.environ.get('PORT', 5000))
 DEBUG = os.environ.get('DEBUG', True) # TODO(ihodes): unsafe, but this doesn't
                                       # matter here.
 
