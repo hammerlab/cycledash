f68b760f7e3c9f501388e0f3ddcc77cf78c946e0
Dan Vanderkam <danvdk@gmail.com>
2014-09-30 12:33:07 -0400

    Add a config setting to log to a file

--- cycledash/__init__.py
+++ cycledash/__init__.py
@@ -11,8 +11,26 @@ def initialize_application():
     return app
 
 
+def _configure_prod_logging(app):
+    @app.errorhandler(500)
+    def internal_error(exception):
+        app.logger.exception(exception)
+        return 'Internal Server Error', 500
+
+    import logging
+    from logging.handlers import RotatingFileHandler
+    file_handler = RotatingFileHandler(app.config['LOG_FILE'],
+            maxBytes=app.config['LOG_FILE_MAXSIZE_BYTES'], backupCount=20)
+    file_handler.setLevel(logging.WARNING)
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    file_handler.setFormatter(formatter)
+    app.logger.addHandler(file_handler)
+
+
 def _configure_application(app):
     app.config.from_object('config')
+    if app.config['LOG_FILE']:
+        _configure_prod_logging(app)
 
 
 app = initialize_application()
