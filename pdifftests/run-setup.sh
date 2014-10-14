c78c39e5759ba1f501714205bb0a3e809c309c36
Dan Vanderkam <danvdk@gmail.com>
2014-10-14 15:52:29 -0400

    delete debug setup script

--- pdifftests/run-setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export PORT=5001
-export DATABASE_URL='sqlite:///../pdifftests/test.db'
-export WEBHDFS_USER=testing
-export WEBHDFS_URL='http://example.com'
-export IGV_HTTPFS_URL='http://example.com'
-export ALLOW_LOCAL_VCFS=true
-python ./run.py
