3ea38afed86e389505849c823e185c91035f190e
Dan Vanderkam <danvdk@gmail.com>
2014-10-16 17:04:53 -0400

    no cat?

--- scripts/travis-test.sh
+++ scripts/travis-test.sh
@@ -2,8 +2,6 @@
 # Run both the Python & JS tests.
 set -o errexit
 
-cat -n /home/travis/build/hammerlab/cycledash/cycledash/static/lib/react/lib/ReactCompositeComponent.js
-
 . ./ENV.sh
 nosetests
 
