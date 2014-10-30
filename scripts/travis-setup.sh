adbcc31e4952b9548f701a5cb545a2b143f2ee52
Dan Vanderkam <danvdk@gmail.com>
2014-10-30 16:42:43 -0400

    remove dpxdt

--- scripts/travis-setup.sh
+++ scripts/travis-setup.sh
@@ -2,7 +2,7 @@
 # Install all deps & build everything for the Python & JS tests.
 set -o errexit
 
-pip install -r <(grep -v 'pysam\|PyVCF' requirements.txt)
+pip install -r <(grep -v 'pysam\|PyVCF\|dpxdt' requirements.txt)
 npm install
 make initenv
 
