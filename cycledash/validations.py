9dc868a4405d177ef164444c4a69f442084c7621
Isaac Hodes <isaachodes@gmail.com>
2014-11-18 13:01:33 -0500

    The great migration: update run validations

--- cycledash/validations.py
+++ cycledash/validations.py
@@ -20,38 +20,35 @@ def Castable(t):
         except:
             raise ValueError
         return v
-    return _Castable()
+    return _Castable(t)
 
 
 CreateRunSchema = Schema({
     Required('variant_caller_name'): unicode,
     Required('vcf_path'): PathString,
-    'truth_vcf_path': PathString,
-    'normal_path': PathString,
-    'tumor_path': PathString,
-    'reference_path': PathString,
+    'normal_bam_path': PathString,
+    'tumor_bam_path': PathString,
+    'is_validation': bool,
     'params': unicode,
     'dataset': unicode,
-    'sha1': unicode
+    'vcf_header': unicode
 })
 
 
 UpdateRunSchema = Schema({
-    'precision': Coerce(float),
-    'recall': Coerce(float),
-    'f1score': Coerce(float),
-    'true_positive': Coerce(int),
-    'false_positive': Coerce(int),
-
     'variant_caller_name': unicode,
     'vcf_path': PathString,
-    'truth_vcf_path': PathString,
-    'normal_path': PathString,
-    'tumor_path': PathString,
-    'reference_path': PathString,
+    'normal_bam_path': PathString,
+    'tumor_bam_path': PathString,
+    'is_validation': bool,
     'params': unicode,
     'dataset': unicode,
-    'sha1': unicode
+    'vcf_header': unicode,
+    'true_positive': Coerce(int),
+    'false_positive': Coerce(int),
+    'precision': Coerce(float),
+    'recall': Coerce(float),
+    'f1score': Coerce(float)
 })
 
 
