5face58b6b4955690e507a0231c8634748f713b6
Isaac Hodes <isaachodes@gmail.com>
2014-11-22 13:37:40 -0500

    Improve styling of the info section of Examine
    Small other improvements in information passing.

---
+++ cycledash/static/js/examine/components/ExamineInformation.js
@@ -0,0 +1,28 @@
+/** @jsx React.DOM */
+"use strict";
+
+var _ = require('underscore'),
+    React = require('react/addons');
+
+
+var ExamineInformation = React.createClass({
+  propTypes: {
+    run:  React.PropTypes.object,
+  },
+  render: function() {
+    var run = this.props.run;
+    return (
+        <div className="examine-information">
+          <dl>
+            <dt>Run ID</dt> <dd>{run.id}</dd>
+            <dt>VCF URI</dt> <dd>{run.uri}</dd>
+            <dt>Caller</dt> <dd>{run.caller_name}</dd>
+            <dt>Dataset</dt> <dd>{run.dataset_name}</dd>
+            <dt>Submitted At</dt> <dd>{run.created_at}</dd>
+          </dl>
+        </div>
+    );
+  }
+});
+
+module.exports = ExamineInformation;
--- cycledash/static/js/examine/examine.js
+++ cycledash/static/js/examine/examine.js
@@ -12,12 +12,8 @@ window.renderExaminePage = function(el, run, igvHttpfsUrl) {
   var dispatcher = new Dispatcher();
   var recordActions = getRecordActions(dispatcher);
   var recordStore = createRecordStore(run.id, dispatcher);
-
   React.renderComponent(<ExaminePage recordStore={recordStore}
                                      recordActions={recordActions}
-                                     vcfPath={run.uri}
-                                     truthVcfPath={run.truthVcfPath}
-                                     normalBamPath={run.normalPath}
-                                     tumorBamPath={run.tumorPath}
+                                     run={run}
                                      igvHttpfsUrl={igvHttpfsUrl} />, el);
 };
