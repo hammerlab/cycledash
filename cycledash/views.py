5a7fe6ac90b5f6b273e94755357fecb9b5e61c06
Isaac Hodes <isaachodes@gmail.com>
2014-12-03 18:53:24 -0500

    This is a style and UX tidying commit
    * Improve Home styling
    * Improve /runs styling
    * Remove /trends and /concordance code
    * Remove plaintext.py, place that information in Home

--- cycledash/views.py
+++ cycledash/views.py
@@ -10,13 +10,8 @@ import requests
 from cycledash import app, db
 import cycledash.genotypes as gt
 from cycledash.helpers import prepare_request_data, update_object
-from cycledash.models import Run, Concordance
-import cycledash.plaintext as plaintext
-from cycledash.validations import (UpdateRunSchema, CreateRunSchema,
-                                   UpdateConcordanceSchema)
+from cycledash.validations import UpdateRunSchema, CreateRunSchema
 
-import workers.concordance
-import workers.scorer
 import workers.indexer
 from workers.genotype_extractor import extract as extract_genotype
 from workers.gene_annotator import annotate as annotate_genes
@@ -32,21 +27,10 @@ RUN_ADDL_KVS = {'Tumor BAM': 'tumor_bam_uri',
 
 @app.route('/')
 def home():
-    if 'text/html' in request.accept_mimetypes:
-        return render_template('home.html')
-    else:
-        return Response(plaintext.HOME_TEXT, mimetype='text/plain')
-
-
-@app.route('/docs/format')
-def format_doc():
-    return Response(plaintext.FORMAT_TEXT, mimetype='text/plain')
+    return render_template('home.html')
 
 
 def start_workers_for_run(run):
-    # if run.truth_vcf_path:
-    #     TODO: This is broken, as run does not have an ID at this point...
-    #     workers.scorer.score.delay(run.id, run.vcf_path, run.truth_vcf_path)
     def index_bai(bam_path):
         workers.indexer.index.delay(bam_path[1:])
     if run.get('normal_path'):
@@ -57,6 +41,7 @@ def start_workers_for_run(run):
     # Run the genotype extractor, and then run the gene annotator with its
     # vcf_id set to the result of the extractor
     chain(extract_genotype.s(json.dumps(run)), annotate_genes.s()).delay()
+
 
 @app.route('/runs', methods=['POST', 'GET'])
 def runs():
@@ -103,89 +88,6 @@ def examine(run_id):
         vcf = dict(con.execute(select_vcf_sql).fetchall()[0])
     return render_template('examine.html', run=dict(vcf))
 
-
-@app.route('/runs/<run_ids_key>/concordance', methods=['GET', 'PUT'])
-def concordance(run_ids_key):
-    runs = [int(run) for run in run_ids_key.split(',')]
-    runs.sort()
-    run_ids_key = ','.join(str(run) for run in runs)
-    concordance = Concordance.query.filter_by(run_ids_key=run_ids_key).first()
-    if request.method == 'PUT':
-        if not concordance:
-            return abort(404)
-        try:
-            data = UpdateConcordanceSchema(prepare_request_data(request))
-        except Exception as e:
-            return jsonify({'error': 'Concordance update validation',
-                            'message': str(e)})
-        update_object(concordance, data)
-        db.session.commit()
-    elif request.method == 'GET':
-        if not concordance:
-            concordance = Concordance(run_ids_key=run_ids_key)
-            db.session.add(concordance)
-            db.session.commit()
-            workers.concordance.concordance.delay(run_ids_key)
-
-    if 'text/html' in request.accept_mimetypes:
-        return render_template('concordance.html', run_ids_key=run_ids_key,
-                               concordance_json=concordance.concordance_json)
-    elif 'application/json' in request.accept_mimetypes:
-        return jsonify(concordance.to_camel_dict())
-
-
-@app.route('/runs/<run_id>', methods=['GET', 'PUT', 'DELETE'])
-def run(run_id):
-    run = Run.query.get_or_404(run_id)
-    if request.method == 'PUT':
-        try:
-            data = UpdateRunSchema(prepare_request_data(request))
-        except Exception as e:
-            return jsonify({'error': 'Run update validation',
-                            'message': str(e)})
-        update_object(run, data)
-        db.session.commit()
-    elif request.method == 'DELETE':
-        db.session.delete(run)
-        db.session.commit()
-    return jsonify(run.to_camel_dict())
-
-
-@app.route('/callers')
-def callers():
-    callers = db.engine.execute("""
-SELECT variant_caller_name, f1score, precision, recall, count(*) AS num_runs
-FROM run
-GROUP BY variant_caller_name
-ORDER BY submitted_at ASC
-""")
-    callers = callers.fetchall()
-    if 'text/html' in request.accept_mimetypes:
-        return render_template('callers.html', callers=callers)
-    else:
-        return jsonify({'callers':
-                        [{'variantCallerName': caller.variant_caller_name,
-                          'f1score': caller.f1score,
-                          'precision': caller.precision,
-                          'recall': caller.recall,
-                          'numRuns': caller.num_runs}
-                         for caller in callers]})
-
-
-@app.route('/callers/<caller_name>')
-def trends(caller_name):
-    runs = Run.query.filter_by(variant_caller_name=caller_name)
-    runs = runs.order_by(Run.submitted_at.desc())
-    runs = sorted((run.to_camel_dict() for run in runs), key=lambda r: r['id'])
-    if 'text/html' in request.accept_mimetypes:
-        datasets = collections.defaultdict(list)
-        for run in runs:
-            datasets[run.get('dataset')].append(run)
-        return render_template('trend.html', runs=runs,
-                               dataset_runs=datasets,
-                               caller_name=caller_name)
-    else:
-        return jsonify({'runs': runs})
 
 # Path must not start with a '/'.
 # Flask seems to have some trouble dealing with forward-slashes in URLs.
