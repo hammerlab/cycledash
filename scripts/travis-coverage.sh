ba1eb04cc496b8585f1d5bfe3a92c639097000df
Dan Vanderkam <danvdk@gmail.com>
2015-03-03 10:46:17 -0500

    Include Python in code coverage numbers

--- scripts/travis-coverage.sh
+++ scripts/travis-coverage.sh
@@ -2,8 +2,24 @@
 # Generates LCOV data to post to coveralls.io when running on Travis.
 set -o errexit
 
+. ./ENV.sh
+
+# Run the Python tests with coverage -- stores results in coverage.xml
+nosetests \
+  --cover-xml \
+  --with-coverage \
+  --cover-tests \
+  --cover-package cycledash,common,workers \
+  tests/python
+
+# Re-run the JavaScript tests with coverage -- stores results in .jscoverage
 NODE_ENV=test ./node_modules/.bin/mocha \
   --require blanket \
   --reporter mocha-lcov-reporter \
   tests/js/*-test.js \
+  > .jscoverage
+
+# Convert coverage.xml to LCOV, merge and post to Coveralls.
+cat .jscoverage \
+  <(./scripts/xml_to_lcov.py coverage.xml) \
   | ./node_modules/coveralls/bin/coveralls.js
