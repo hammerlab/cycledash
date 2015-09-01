a45bfaaebe301292b7ec9b1b7becfe04210e762f
B. Arman Aksoy <arman@aksoy.org>
2015-09-01 12:20:34 -0400

    clean the alembic script up and better annotate it

--- alembic/versions/2c00861f3ff1_vcfs_store_inferred_ensembl_release.py
+++ alembic/versions/2c00861f3ff1_vcfs_store_inferred_ensembl_release.py
@@ -1,7 +1,8 @@
-"""vcfs: store inferred ENSEMBL release
+"""Add vcf_release column to the vcfs table to store the inferred ENSEMBL
+release identifier that follows PyEnsembl and Varcode conventions.
 
 Revision ID: 2c00861f3ff1
-Revises: 
+Revises:
 Create Date: 2015-08-31 17:50:09.263094
 
 """
@@ -18,9 +19,6 @@ import sqlalchemy as sa
 
 def upgrade():
     op.add_column('vcfs', sa.Column('vcf_release', sa.Integer))
-    pass
-
 
 def downgrade():
     op.drop_column('vcfs', 'vcf_release')
-    pass
